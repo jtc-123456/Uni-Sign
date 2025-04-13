360bbfc3c059d93f94b1caf87a7f720f35aa583a
jtc-123456 <1436542418@qq.com>
2025-04-13 10:26:06 +0000

    Update from Colab

--- config.py
+++ config.py
@@ -2,19 +2,22 @@ mt5_path =  "./pretrained_weight/mt5-base"
 
 # label paths
 train_label_paths = {
-                    "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
+                    # "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
+                    "CSL_News": "/content/xiaomi.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.train",
                     "WLASL": "./data/WLASL/labels-2000.train"
                     }
 
 dev_label_paths = {
-                    "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
+                    # "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
+                    "CSL_News": "/content/xiaomi.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.dev",
                     "WLASL": "./data/WLASL/labels-2000.dev"
                     }
 
 test_label_paths = {
-                    "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
+                    "CSL_News": "/content/xiaomi.json",
+                    # "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
                     "CSL_Daily": "./data/CSL_Daily/labels.test",
                     "WLASL": "./data/WLASL/labels-2000.test"
                     }
@@ -22,14 +25,16 @@ test_label_paths = {
 
 # video paths
 rgb_dirs = {
-            "CSL_News": './data/CSL_News/rgb_format',
+            # "CSL_News": './data/CSL_News/rgb_format',
+            "CSL_News": '/content',
             "CSL_Daily": './dataset/CSL_Daily/sentence-crop',
             "WLASL": "./dataset/WLASL/rgb_format"
             }
 
 # pose paths
 pose_dirs = {
-            "CSL_News": './data/CSL_News/pose_format',
+            # "CSL_News": './data/CSL_News/pose_format',
+            "CSL_News": '/content',
             "CSL_Daily": './dataset/CSL_Daily/pose_format',
             "WLASL": "./dataset/WLASL/pose_format"
             }
