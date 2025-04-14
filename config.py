5bd6dc3cce735edb7d24574f7426a1b6b7803988
jtc-123456 <1436542418@qq.com>
2025-04-14 13:06:14 +0000

    Update from Colab

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 mt5_path =  "./pretrained_weight/mt5-base"
-dataset='csl-news'
+dataset='vivo'
 # label paths
 train_label_paths = {
                     # "CSL_News": "./data/CSL_News/CSL_News_Labels.json",
